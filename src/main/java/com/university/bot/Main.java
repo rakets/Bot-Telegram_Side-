9490be2fc2e5cfbace32bb4f9f7cabd86ee2365b
Ushkevich Valiantsin <vushkevih@gmail.com>
2024-12-18 17:16:56 +0100

    Basig functional of bot

--- src/main/java/com/university/bot/Main.java
+++ src/main/java/com/university/bot/Main.java
@@ -1,7 +1,29 @@
 package com.university.bot;
 
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+//import org.telegram.telegrambots.exception.TelegramApiException;
+//import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println();
+    public static void main(String[] args){
+//        // TODO Instantiate Telegram API
+//        TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+//
+//        // TODO Register our bot
+//        String botToken = "7713025774:AAFjV0AnEKhW9Mm4AsPLh0O8abxIK19tfOk";
+//        botsApplication.registerBot(botToken, new routeBot(botToken));
+//        System.out.println("Bot successfully started!");
+
+//        Instantiate Telegram API
+//        TODO Register our bot
+        String botToken = "7713025774:AAFjV0AnEKhW9Mm4AsPLh0O8abxIK19tfOk";
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new routeBot(botToken));
+            System.out.println("Bot successfully started!");
+            // Ensure this prcess wait forever
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
